fa9138d0d8a51edfcc2ce5b159ff4ba713b04e64
breeze <breeze@abc.com>
2019-03-20 12:06:39 +0700

    RegOK

---
+++ AuthenSSRU/Regis.swift
@@ -0,0 +1,30 @@
+//
+//  Regis.swift
+//  AuthenSSRU
+//
+//  Created by Student07 on 20/3/2562 BE.
+//  Copyright © 2562 Student07. All rights reserved.
+//
+
+import UIKit
+
+class Regis: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
